07bdcc479dd79a80d57d06a6b8041ada54101c6a
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-02-09 19:49:12 +0530

    added over button effect and a blank play screen

--- gui.py
+++ gui.py
@@ -15,12 +15,14 @@ intro_music = pygame.mixer.Sound("audio/intro.ogg")
 start_music = pygame.mixer.Sound("audio/start.ogg")
 play_img = pygame.image.load("images/play.png")
 quit_img = pygame.image.load("images/quit.png")
+quit_img_over = pygame.image.load("images/quit_over.png")
+play_img_over = pygame.image.load("images/quit_over.png")
 pygame.display.set_icon(icon)
 
 window.blit(intro_img, (width // 2 - intro_img.get_width() // 2, height // 2 - intro_img.get_height() // 2))
 pygame.display.update()
-pygame.mixer.Sound.play(intro_music)
-pygame.time.delay(5000)
+# pygame.mixer.Sound.play(intro_music)
+# pygame.time.delay(5000)
 
 quit_img_loc = (play_img.get_width(), 500)
 play_img_loc = (0, 500)
@@ -46,28 +48,49 @@ quit_txt_rect = quit_txt.get_rect()
 play_txt_rect.center = (300, 560)
 quit_txt_rect.center = (900, 560)
 
-def start_window():
+def game_window():
+    # pygame.mixer.Sound.play(start_music)
     running = True
     while running:
+        window.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
+        pygame.display.update()
+
+
+def start_window():
+    pos = (0, 0)
+    while True:
         window.blit(quit_img, quit_img_loc)
         window.blit(play_img, play_img_loc)
-        window.blit(play_txt, play_txt_rect)
-        window.blit(quit_txt, quit_txt_rect)
+
+        if is_over(pos, quit_img_loc, quit_img.get_size(), 60):
+            window.blit(quit_img_over, quit_img_loc)
+        
+        if is_over(pos, play_img_loc, play_img.get_size(), 60):
+            window.blit(play_img_over, play_img_loc)
 
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return 0
+
+            pos = pygame.mouse.get_pos()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
 
                 if is_over(pos, play_img_loc, play_img.get_size(), 60):
-                    pygame.mixer.Sound.play(start_music)
                     print("Start")
+                    game_window()
+                    return 0
 
                 if is_over(pos, quit_img_loc, quit_img.get_size(), 60):
-                    running = False
+                    return 0
+
+        window.blit(play_txt, play_txt_rect)
+        window.blit(quit_txt, quit_txt_rect)
 
         pygame.display.update()
 
