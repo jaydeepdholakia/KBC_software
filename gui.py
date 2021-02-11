70f51578b79b55b7a3202de76448dd9dc0737630
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-02-11 13:26:46 +0530

    Test commit for finish branch

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
+import pygame
 import os
 import quiz
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame 
 pygame.init()
 
 width = 1200
@@ -51,48 +51,51 @@ opt_b_img_correct = pygame.image.load("images/opt_r_correct.png")
 opt_c_img_correct = pygame.image.load("images/opt_l_correct.png")
 opt_d_img_correct = pygame.image.load("images/opt_r_correct.png")
 
-pygame.display.set_icon(icon) 
+pygame.display.set_icon(icon)
 
-window.blit(intro_img, (0, 0)) 
-pygame.display.update() 
-pygame.mixer.Sound.play(intro_music) 
-pygame.time.delay(6500) 
+window.blit(intro_img, (0, 0))
+pygame.display.update()
+pygame.mixer.Sound.play(intro_music)
+pygame.time.delay(6500)
 
-quit_img_loc = (play_img.get_width(), 500) 
-play_img_loc = (0, 500) 
+quit_img_loc = (play_img.get_width(), 500)
+play_img_loc = (0, 500)
 
-opt_a_img_loc = (0, 400) 
-opt_b_img_loc = (600, 400) 
-opt_c_img_loc = (0, 500) 
-opt_d_img_loc = (600, 500) 
+opt_a_img_loc = (0, 400)
+opt_b_img_loc = (600, 400)
+opt_c_img_loc = (0, 500)
+opt_d_img_loc = (600, 500)
 
 running = True
 
-def is_over(pos, up, down, trim): 
-    up = (up[0]+trim, up[1]) 
-    down = (down[0]-trim, down[1]) 
-    if pos[0] > up[0] and pos[1] > up[1]: 
-        if pos[0] < up[0] + down[0] and pos[1] <  up[1] + down[1]:
-            return True 
-    else:
-        return False 
 
-white = (255, 255, 255) 
-green = (0, 255, 0) 
-blue = (0, 0, 128) 
-gold = (218,165,32) 
+def is_over(pos, up, down, trim):
+    up = (up[0]+trim, up[1])
+    down = (down[0]-trim, down[1])
+    if pos[0] > up[0] and pos[1] > up[1]:
+        if pos[0] < up[0] + down[0] and pos[1] < up[1] + down[1]:
+            return True
+    else:
+        return False
+
+
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+gold = (218, 165, 32)
 black = (0, 0, 0)
-grey = (90, 90, 90) 
+grey = (90, 90, 90)
 intro_font = pygame.font.Font('fonts/RobotoSlab-Medium.ttf', 80)
 question_font = pygame.font.Font('fonts/RobotoSlab-Medium.ttf', 30)
 opt_char_font = pygame.font.Font('fonts/RobotoSlab-SemiBold.ttf', 40)
 opt_font = pygame.font.Font('fonts/RobotoSlab-Medium.ttf', 35)
-play_txt = intro_font.render('Play', True, white) 
-quit_txt = intro_font.render('Quit', True, white) 
-play_txt_rect = play_txt.get_rect() 
-quit_txt_rect = quit_txt.get_rect() 
-play_txt_rect.center = (300, 560) 
-quit_txt_rect.center = (900, 560) 
+play_txt = intro_font.render('Play', True, white)
+quit_txt = intro_font.render('Quit', True, white)
+play_txt_rect = play_txt.get_rect()
+quit_txt_rect = quit_txt.get_rect()
+play_txt_rect.center = (300, 560)
+quit_txt_rect.center = (900, 560)
+
 
 def update():
 
@@ -107,15 +110,15 @@ def update():
     opt_c_char_txt = opt_char_font.render("C:", True, gold)
     opt_d_char_txt = opt_char_font.render("D:", True, gold)
 
-    opt_a_char_txt_rect = opt_a_char_txt.get_rect() 
-    opt_b_char_txt_rect = opt_b_char_txt.get_rect() 
-    opt_c_char_txt_rect = opt_c_char_txt.get_rect() 
-    opt_d_char_txt_rect = opt_d_char_txt.get_rect() 
+    opt_a_char_txt_rect = opt_a_char_txt.get_rect()
+    opt_b_char_txt_rect = opt_b_char_txt.get_rect()
+    opt_c_char_txt_rect = opt_c_char_txt.get_rect()
+    opt_d_char_txt_rect = opt_d_char_txt.get_rect()
 
-    opt_a_char_txt_rect.center = (120, 435) 
-    opt_b_char_txt_rect.center = (680, 435) 
-    opt_c_char_txt_rect.center = (120, 540) 
-    opt_d_char_txt_rect.center = (680, 540) 
+    opt_a_char_txt_rect.center = (120, 435)
+    opt_b_char_txt_rect.center = (680, 435)
+    opt_c_char_txt_rect.center = (120, 540)
+    opt_d_char_txt_rect.center = (680, 540)
 
     quiz_data = quiz.get_question(difficulty)
 
@@ -133,24 +136,24 @@ def update():
         question_txt_1 = question_font.render(question[0], True, white)
         question_txt_2 = question_font.render(question[1], True, white)
 
-    opt_a_txt = opt_font.render(opt_a, True, white) 
-    opt_b_txt = opt_font.render(opt_b, True, white) 
-    opt_c_txt = opt_font.render(opt_c, True, white) 
-    opt_d_txt = opt_font.render(opt_d, True, white) 
+    opt_a_txt = opt_font.render(opt_a, True, white)
+    opt_b_txt = opt_font.render(opt_b, True, white)
+    opt_c_txt = opt_font.render(opt_c, True, white)
+    opt_d_txt = opt_font.render(opt_d, True, white)
 
     if len(question) == 1:
-        question_txt_rect = question_txt.get_rect() 
+        question_txt_rect = question_txt.get_rect()
     else:
         question_txt_1_rect = question_txt_1.get_rect()
         question_txt_2_rect = question_txt_2.get_rect()
 
-    opt_a_txt_rect = opt_a_txt.get_rect() 
-    opt_b_txt_rect = opt_b_txt.get_rect() 
-    opt_c_txt_rect = opt_c_txt.get_rect() 
-    opt_d_txt_rect = opt_d_txt.get_rect() 
+    opt_a_txt_rect = opt_a_txt.get_rect()
+    opt_b_txt_rect = opt_b_txt.get_rect()
+    opt_c_txt_rect = opt_c_txt.get_rect()
+    opt_d_txt_rect = opt_d_txt.get_rect()
 
     if len(question) == 1:
-        question_txt_rect.center = (610, 260) 
+        question_txt_rect.center = (610, 260)
     else:
         question_txt_1_rect.center = (610, 235)
         question_txt_2_rect.center = (610, 280)
@@ -161,17 +164,16 @@ def update():
     opt_d_txt_rect.center = (705 + opt_d_txt_rect.width // 2, 540)
 
     opt_data = {
-    "a": [opt_a, opt_a_img_lock, opt_a_img_wrong, opt_a_img_correct, opt_a_img_loc, opt_a_txt_rect, opt_a_char_txt_rect],
-    "b": [opt_b, opt_b_img_lock, opt_b_img_wrong, opt_b_img_correct, opt_b_img_loc, opt_b_txt_rect, opt_b_char_txt_rect],
-    "c": [opt_c, opt_c_img_lock, opt_c_img_wrong, opt_c_img_correct, opt_c_img_loc, opt_c_txt_rect, opt_c_char_txt_rect],
-    "d": [opt_d, opt_d_img_lock, opt_d_img_wrong, opt_d_img_correct, opt_d_img_loc, opt_d_txt_rect, opt_d_char_txt_rect]
+        "a": [opt_a, opt_a_img_lock, opt_a_img_wrong, opt_a_img_correct, opt_a_img_loc, opt_a_txt_rect, opt_a_char_txt_rect],
+        "b": [opt_b, opt_b_img_lock, opt_b_img_wrong, opt_b_img_correct, opt_b_img_loc, opt_b_txt_rect, opt_b_char_txt_rect],
+        "c": [opt_c, opt_c_img_lock, opt_c_img_wrong, opt_c_img_correct, opt_c_img_loc, opt_c_txt_rect, opt_c_char_txt_rect],
+        "d": [opt_d, opt_d_img_lock, opt_d_img_wrong, opt_d_img_correct, opt_d_img_loc, opt_d_txt_rect, opt_d_char_txt_rect]
     }
     # pygame.mixer.Sound.play(start_music)
 
 
-
-
 update()
+
 
 def check(opt, ans):
     global question_no, difficulty
@@ -192,7 +194,7 @@ def check(opt, ans):
             difficulty = 2
         pygame.display.update()
         pygame.time.delay(1000)
-    
+
     else:
         window.blit(opt_data[opt][2], opt_data[opt][4])
         window.blit(opt_data[ans][3], opt_data[ans][4])
@@ -213,7 +215,6 @@ def check(opt, ans):
 
     update()
     return True
-
 
 
 def lock(opt):
@@ -292,23 +293,22 @@ def game_window():
                 if is_over(pos, opt_d_img_loc, opt_d_img.get_size(), 55):
                     lock('d')
                     running = check('d', answer)
-                
+
         pygame.display.update()
 
 
 def start_window():
     pos = (0, 0)
     while True:
-        window.blit(intro_img, (0, 0)) 
+        window.blit(intro_img, (0, 0))
         window.blit(quit_img, quit_img_loc)
         window.blit(play_img, play_img_loc)
 
         if is_over(pos, quit_img_loc, quit_img.get_size(), 60):
             window.blit(quit_img_over, quit_img_loc)
-        
+
         if is_over(pos, play_img_loc, play_img.get_size(), 60):
             window.blit(play_img_over, play_img_loc)
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -331,3 +331,4 @@ def start_window():
 
 
 start_window()
+# test
